b6f839bf64ac26d57e54566fc7c6141538bbb343
juliodeoliveira <ramosju423@gmail.com>
2025-03-31 19:57:08 -0300

    feat: Validating if user can book a room

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -17,7 +17,36 @@ class ClientController extends Controller
             'check_in_date' => 'required|date|date_format:Y-m-d', 
             'check_out_date' => 'required|date|date_format:Y-m-d|after:check_in_date', 
             'room_number' => 'required|integer|exists:rooms,room_number',
+        ], [
+            "name.required" => "invalid name",
+            "check_in_date.required" => "invalid check in date",
+            "check_in_date.date_format" => "invalid check in date",
+            "check_out_date.required" => "invalid check out date",
+            "check_ou_date.date_format" => "invalid check out date",
+            "room_number.exists" => "invalid room number",
         ]);
+
+        function isValidDate($date, $format = 'Y-m-d') {
+            $d = \DateTime::createFromFormat($format, $date);
+            return $d && $d->format($format) === $date;
+        }
+
+        $clientsBooks = Clients::select("check_in_date", "check_out_date")->where("room_number", $validateData["room_number"])->get();
+        
+        if (! $clientsBooks->isEmpty()) {
+            foreach ($clientsBooks as $book) {
+                $checkInDate = $book->check_in_date;
+                $checkOutDate = $book->check_out_date;
+    
+                $checkOutDate = new \DateTime($checkOutDate);
+                $checkInDate = new \DateTime($checkInDate);
+                $inputCheckIn = new \DateTime($validateData["check_in_date"]);
+    
+                if ($inputCheckIn >= $checkInDate && $inputCheckIn <= $checkOutDate) {
+                    return response()->json(["reserved"], 403);
+                }
+            }
+        }
 
         $bookARoom = Clients::create([
             'name'=> $validateData["name"],
@@ -26,6 +55,6 @@ class ClientController extends Controller
             'room_number'=> $validateData["room_number"],
         ]);
 
-        return response()->json([$bookARoom], 201);
+        return response()->json(["success"], 201);
     }
 }
