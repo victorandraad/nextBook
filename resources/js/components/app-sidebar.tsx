4424f2656e10152e87247d8b8fc5a5c4e0308588
Victor Daniel Andrade da Silva <68758638+victorandraad@users.noreply.github.com>
2025-04-14 20:35:40 -0300

    Página inicial

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -16,16 +16,7 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
-    },
+    
 ];
 
 export function AppSidebar() {
