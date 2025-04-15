953398548880c4c87fc1fd344cb992a9be7af624
Marcus-Vi <152079038+Marcus-Vi@users.noreply.github.com>
2025-04-15 14:42:07 -0300

    bug corrigido

--- resources/js/components/pop-up.tsx
+++ resources/js/components/pop-up.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, Di
 
 interface PopUpProps {
     triggerText?: string;
+    roomNumber?: number;
     caseStyle?: string;
     children: React.ReactNode;
     contentStyle?: string;
@@ -13,9 +14,9 @@ export function PopUp(props: PopUpProps): React.ReactElement {
             <DialogTrigger className={`cursor-pointer rounded-b-md ${props.caseStyle}`}>{props.triggerText}</DialogTrigger>
             <DialogContent className="sm:max-w-[950px] sm:h-[780px]">
                 <DialogHeader>
-                    <DialogTitle>{props.triggerText}</DialogTitle>
+                    <DialogTitle>Quarto {props.roomNumber}</DialogTitle>
                     <DialogDescription>
-                        Detalhes e informações do {props.triggerText}
+                        Detalhes e informações do {props.roomNumber}
                     </DialogDescription>
                     <div>
                         {props.children}
